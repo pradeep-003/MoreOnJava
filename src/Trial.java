41e98c7f34ef9ce0c5546671af41e07f32f83d04
Pradeep-003 <pradeep50953@gmail.com>
2023-08-25 10:33:18 +0530

    Starting Milestone3onjava.imltone

--- src/Trial.java
+++ src/Trial.java
@@ -1,13 +1,9 @@
 import java.util.ArrayList;
-import java.util.*;
+
 public class Trial {
+    public static void main(String[] args) {
 
-        public static void main(String[] args) {
+    }
+}
 
-            List<Integer> intList = new ArrayList<>();
 
-            intList.add(10);
-
-            intList.add(20);
-
-            System.out.println("The list is: " + intList);}}
