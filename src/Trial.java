e82c6495fb7dee14a34e5b07dd1df3fc418e7e3b
Pradeep-003 <pradeep50953@gmail.com>
2023-08-26 10:49:14 +0530

    Starting Milestone3onjava.imltone

--- src/Trial.java
+++ src/Trial.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Trial {
     public static void main(String[] args) {
-
+       Scanner sc= new Scanner(System.in);
+       sc.nextInt();
     }
 }
 
